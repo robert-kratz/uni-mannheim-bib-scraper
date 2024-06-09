00ebf7a06030588b21ca1831d92fbfe4a3058aa2
robert.kratz <robert.kratz@students.uni-mannheim.de>
2024-06-09 23:08:27 +0200

    Add Google Analytics tracking code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    publicRuntimeConfig: {
+        GA_TAG: 'G-5D96HFBJ4V',
+    },
 };
 
 export default nextConfig;
